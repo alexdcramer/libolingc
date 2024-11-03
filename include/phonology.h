af379db747b828d1ee5bf4782ab7bbe13b327ea4
N3ther <alex@consus.fun>
2024-11-03 09:27:34 -0500

    improved documentation

--- include/phonology.h
+++ include/phonology.h
@@ -29,20 +29,44 @@ typedef struct phono_system {
 }phono_system;
 
 
-// IPA function
+// Create a phono_system with the same structure as the IPA
 extern struct phono_system* generate_ipa();
 
-// Initializing functions
+/**
+ * ==========================
+ * | Initializing Functions |
+ * ==========================
+ **/
+
+// Create a blank phono_cell containing one blank phoneme
 extern struct phono_cell* init_blank_cell();
+
+// Create a blank phono_row containing one blank cell
 extern struct phono_row* init_blank_row();
+
+// Create a blank phono_table containing one blank row
 extern struct phono_table* init_blank_table();
+
+// Create a blank phono_system containing one blank table
 extern struct phono_system* init_blank_system();
 
-// Freeing functions
+/**
+ * =====================
+ * | Freeing Functions |
+ * =====================
+ **/
+
+// The following functions are required for use as to prevent memory leaks. free() likely will not work on these objects.
+
+// Free a phoneme from memory.
 extern void free_phoneme(struct phoneme* p);
+// Free a phono_cell from memory.
 extern void free_phono_cell(struct phono_cell* c);
+// Free a phono_row from memory.
 extern void free_phono_row(struct phono_row* r);
+// Free a phono_table from memory.
 extern void free_phono_table(struct phono_table* t);
+// Free a phono_system from memory.
 extern void free_phono_system(struct phono_system* s);
 
 #endif
