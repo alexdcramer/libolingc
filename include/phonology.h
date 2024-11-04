74e05bc2dc58a2b7521a670f392ee4f8884163d6
N3ther <alex@consus.fun>
2024-11-04 03:21:02 -0500

    added blank initializers and freeing functions

--- include/phonology.h
+++ include/phonology.h
@@ -1,31 +1,59 @@
 #ifndef _LIBOLINGC_PHONOLOGY_H_
 #define _LIBOLINGC_PHONOLOGY_H_
 
+/**
+ * The following typedefs are for components of a phono_system
+ *
+ * A phono_system can be thought of as simply a list of 3D tables, combined with metadata
+ * 
+ * Each phono_system is made of 1 or more instances of phono_table
+ * 
+ * A phono_table is a three-dimentional table containing 1 or more instances of phono_row, alongside some information about the sounds it contains
+ * 
+ * These can be thought of as the consonant and vowel tables of the IPA. Non-pulmonics can also be stored in a table, but there isn't any columnar data
+ * needed for those.
+ * 
+ * Each table contains n amount of phono_rows. These can be thought of as rows in the IPA consonant table (plosives, nasals, etc.)
+ * phono_rows have their size defined by their parent, as all rows need to be the same size. This does cause some data to be used for empty cells,
+ * but it should be fine.
+ *
+ * Each row contains n amount of phono_cells. These can be thought of as the unvoiced/voiced pairs in the IPA.
+ *
+ * Each cell contains n amount of phonemes. These can be thought of as the individual characters of the IPA.
+ *
+ * Preferably, def strings should point back to the strings defined by the table
+ *
+ */
 typedef struct phoneme {
-	char* value, def, cell_def, row_def, table_def;
+	char* value;
+	char* def; // for IPA: voiced/unvoiced
+	struct phono_cell* parent;
 }phoneme;
 
 typedef struct phono_cell {
-	struct phoneme** phonemes;
-	char* def, row_def, table_def;
+	char* def; // for IPA: bilabial/labiodental/.../glottal
+	phoneme** phonemes;
+	struct phono_row* parent;
 }phono_cell;
 
 typedef struct phono_row {
-	struct phono_cell** cells;
-	int* cell_size;
-	char* def, table_def;
+	char* def; // for IPA: plosive/nasal/.../lateral approximant
+	phono_cell** cells;
+
+	struct phono_table* parent;
 }phono_row;
 
 typedef struct phono_table {
-	struct phono_row** rows;
-	int* row_size, num_rows;
-	char* def;
+	phono_row** rows;
+	int *row_size, *num_rows, *cell_size;
+	char **row_defs, **column_defs, **cell_defs;
+	struct phono_system* parent;
 }phono_table;
 
 typedef struct phono_system {
-	struct phono_table** tables;
+	phono_table** tables;
 	char** diacritics;
-	int num_tables;
+	int *num_tables, *num_diacritics;
 }phono_system;
 
 
--- src/phonology.c
+++ src/phonology.c
@@ -1,5 +1,12 @@
 #include <stdlib.h>
+#include <strings.h>
 #include "phonology.h"
+
+/**
+ * =================================
+ * | Blank Datatype Initialization |
+ * =================================
+ */
 
 phono_cell* init_blank_cell() {
 	phoneme* p = malloc(sizeof(phoneme));
@@ -8,9 +15,141 @@ phono_cell* init_blank_cell() {
 
 	phono_cell* c = malloc(sizeof(phono_cell));
 	c->phonemes = &p;
-	c->def = malloc(sizeof(char));
 	
-	p->cell_def = c->def;
-
 	return c;
+}
+
+phono_row* init_blank_row() {
+	
+	phono_row* r = malloc(sizeof(phono_row));
+
+	phono_cell* c = init_blank_cell();
+	c->parent = r;
+
+	r->cells = &c;
+	r->def = malloc(sizeof(char));
+	
+	return r;
+}
+
+phono_table* init_blank_table() {
+
+	phono_table* t = malloc(sizeof(phono_table));
+
+	phono_row* r = init_blank_row();
+	r->parent = t;
+
+	t->rows = &r;
+	int* rowsize = malloc(sizeof(int));
+	int* numrows = malloc(sizeof(int));
+	int* cellsize = malloc(sizeof(int));
+	*rowsize = 1;
+	*numrows = 1;
+	*cellsize = 1;
+	t->row_size = rowsize;
+	t->num_rows = numrows;
+	t->cell_size = cellsize;
+
+	char* rowdef = malloc(sizeof(char));
+	char* columndef = malloc(sizeof(char));
+	char* celldef = malloc(sizeof(char));
+	t->row_defs = &rowdef;
+	t->column_defs = &columndef;
+	t->cell_defs = &celldef;
+
+	// set blank children defs to properly point to table defs
+	free(r->def);
+	r->def = rowdef;
+
+	phono_cell* c = r->cells[0];
+	free(c->def);
+	c->def = columndef;
+
+	phoneme* p = c->phonemes[0];
+	free(p->def);
+	p->def = celldef;
+
+	return t;
+}
+
+phono_system* init_blank_system() {
+
+	phono_system* s = malloc(sizeof(phono_system));
+	char* diacritics = malloc(sizeof(char));
+	s->diacritics = &diacritics;
+	
+	int* numtables = malloc(sizeof(int));
+	*numtables = 1;
+	s->num_tables = numtables;
+	
+	phono_table* t = init_blank_table();
+	s->tables = &t;
+	
+	return s;
+}
+
+/**
+ * =====================
+ * | Freeing Functions |
+ * =====================
+ */
+
+void free_phoneme(phoneme* p) {
+	free(p->value);
+	free(p);
+}
+
+void free_phono_cell(phono_cell* c) {
+	for (int i = 0; i < *(c->parent->parent->cell_size); i++) {
+		free_phoneme(c->phonemes[i]);
+	}
+	free(c);
+}
+
+void free_phono_row(phono_row* r) {
+	for (int i = 0; i < *(r->parent->row_size); i++) {
+		free_phono_cell(r->cells[i]);
+	}
+	free(r);
+}
+
+void free_phono_table(phono_table* t) {
+	for (int i = 0; i < *(t->num_rows); i++) {
+		free_phono_row(t->rows[i]);
+	}
+	
+	for (int i = 0; i < *(t->row_size); i++) {
+		free(t->column_defs[i]);
+	}
+	for (int i = 0; i < *(t->num_rows); i++) {
+		free(t->row_defs[i]);
+	}
+	for (int i = 0; i < *(t->cell_size); i++) {
+		free(t->cell_defs[i]);
+	}
+
+	free(t->row_size);
+	free(t->num_rows);
+	free(t->cell_size);
+
+	free(t->row_defs);
+	free(t->column_defs);
+	free(t->cell_defs);
+
+	free(t);
+}
+
+void free_phono_system(phono_system* s) {
+	for (int i = 0; i < *(s->num_tables); i++) {
+		free_phono_table(s->tables[i]);
+	}
+
+	for (int i = 0; i < *(s->num_diacritics); i++) {
+		free(s->diacritics[i]);
+	}
+
+	free(s->num_tables);
+	free(s->num_diacritics);
+
+	free(s);
 }
