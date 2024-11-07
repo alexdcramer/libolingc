d3c8a27bc6667ae51cdf598f98fc7e9418616f6f
N3ther <alex@consus.fun>
2024-11-07 00:58:22 -0500

    Started work on testing binary

--- Makefile
+++ Makefile
@@ -1,5 +1,12 @@
-
+bin/test-libolingc: lib/libolingc.a src/test/test-phonology.c
+	$(info Creating testing binary...)
+	gcc -o bin/test-libolingc -I ./include src/test/test-phonology.c lib/libolingc.a
+lib/libolingc.a: obj/phonology.o
+	$(info Creating static library...)
+	ar rcs lib/libolingc.a obj/phonology.o
 lib/libolingc.so: obj/phonology.o
-	gcc -v -shared -o lib/libolingc.so obj/phonology.o
+	$(info Creating shared library...)
+	gcc -shared -o lib/libolingc.so obj/phonology.o
 obj/phonology.o: src/phonology.c include/phonology.h
-	gcc -v -fPIC -o obj/phonology.o -I ./include -c src/phonology.c
+	$(info Creating objects...)
+	gcc -fPIC -o obj/phonology.o -I ./include -c src/phonology.c
---
+++ src/test/test-phonology.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <phonology.h>
+
+int test_blank_cell() {
+	int fail_count = 0;
+	phono_cell* blank_cell = init_blank_cell();
+	
+	if (!blank_cell->phonemes[0]) {
+		printf("Phoneme was not created in blank cell!\n");
+		fail_count += 1;
+	} else {
+		phoneme* p = blank_cell->phonemes[0];
+		free_phono_cell(blank_cell);
+		if (p != NULL) {
+			printf("Phoneme was not properly freed from blank cell!\n");
+			fail_count += 1;
+		}
+	}	
+
+	return fail_count;
+}
+
+int test_blank_inits() {
+	int fail_count = 0;
+	printf("Testing blank structures...\n");
+	
+	fail_count += test_blank_cell();
+
+	return fail_count;
+}
+
+int main(int argc, char* argv[]) {
+
+	int fail_count = 0;
+	fail_count += test_blank_inits();
+
+	return fail_count;
+}
